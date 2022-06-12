a40c8487be9f5846eadccbcf6524c13d640055c1
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-06-12 16:31:00 +0100

    Started Header

---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import Navbar from "./navbar/Navbar"
+
+const Header = () => {
+  return (
+    <header>
+        <h1>Task Tracker</h1>
+        <Navbar />
+        <h2>The best task tracker on the interwebs!</h2>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/header/navbar/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Tasks</li>
+            <li>L.M.Broadhurst</li>
+            <li>Footer</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
